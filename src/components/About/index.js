366875fdbaf60f9f36a196e1eb888f88bedfe239
Vue Lee <vuelee09@gmail.com>
2022-07-20 14:53:12 -0500

    render gallary pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,14 @@ function About() {
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+            <div className="my-2">
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                praesentium ullam quod esse quidem repellendus provident dolores. 
+                Aperiam perferendis rerum, perspiciatis deserunt qui necessitatibus 
+                sunt fuga nisi sint deleniti quasi?
+                </p>
+            </div>
         </section>
     );
 }
